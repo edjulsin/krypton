2e87c79b52cd7174ed4bc7f99f5e5c52aaa9c06f
edjulsin <edjulsin@gmail.com>
2024-04-17 05:37:06 +0700

    simplify socket retry and reduce delay time

--- src/js/utils/retrySocket.js
+++ src/js/utils/retrySocket.js
@@ -1,4 +1,4 @@
-import { fromEvent, timer, retry, concatMap, iif, of, catchError } from 'rxjs'
+import { fromEvent, timer, retry, concatMap, iif, catchError } from 'rxjs'
 import { fromFetch } from 'rxjs/fetch'
 
 const retrySocket = retry({
@@ -6,16 +6,17 @@ const retrySocket = retry({
     delay: (error, count) => {
         if(count < 5) {
             const idle = () => document.visibilityState === 'hidden'
-            const onInternet = fromFetch('/platform/status')
-            const onActive = fromEvent(document, 'visibilitychange').pipe(
-                concatMap(() => onInternet)
-            )
-            return timer(15000).pipe(
-                concatMap(() =>
-                    iif(idle, onActive, onInternet)
-                ),
+            const onLine = fromFetch('/platform/status').pipe(
                 catchError(() =>
                     timer(60000 * count)
+                )
+            )
+            const onActive = fromEvent(document, 'visibilitychange').pipe(
+                concatMap(() => onLine)
+            )
+            return timer(5000).pipe(
+                concatMap(() =>
+                    iif(idle, onActive, onLine)
                 )
             )
         } else {
